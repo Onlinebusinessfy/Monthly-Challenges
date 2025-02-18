7c10ab43a7a8df4f98a846f975f4af06d2f28e51
Onlinebusinessfy <onlinebusinessfy@gmail.com>
2025-02-18 10:09:02 -0800

    Trabajo en clase del 18 de febrero

--- challenges/views.py
+++ challenges/views.py
@@ -1,10 +1,61 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
-def index(request): 
-    return HttpResponse("Este es un mensaje desde el index.")
+def index(request):
+    return HttpResponse("Bienvenido")
 
+def monthly_challenge(request, month): 
+    if month=='january':
+        return HttpResponse("Walk for at least 30 min every day.")
+    elif month=='february':
+        return HttpResponse("Go to the gym every day.")
+    elif month=='march':
+        return HttpResponse("Shower every day.")
+    elif month=='april':
+        return HttpResponse("Play soccer every day.")
+    elif month=='may':
+        return HttpResponse("Hang out with friends.")
+    elif month=='june':
+        return HttpResponse("Attend to school every day.")
+    elif month=='july':
+        return HttpResponse("Sleep at least 8 hours every day.")
+    elif month=='august':
+        return HttpResponse("Enjoy vacations.")
+    elif month=='september':
+        return HttpResponse("Finish all my tasks before the due date.")
+    elif month=='october':
+        return HttpResponse("Enjoy the time with my family every day.")
+    elif month=='november':
+        return HttpResponse("Start a personal project.")
+    elif month=='december':
+        return HttpResponse("Celebrate Christmas and New Year.")
+    else:
+        return HttpResponseNotFound("Month not found")
+
+
+def dictionary(request, month):
+    diccionario={
+        "january":"Walk for at least 30 min every day.",
+        "february":"Go to the gym every day.",
+        "march":"Shower every day.",
+        "april":"Play soccer every day.",
+        "may":"Hang out with friends.",
+        "june":"Attend to school every day.",
+        "july":"Sleep at least 8 hours every day.",
+        "august":"Enjoy vacations.",
+        "september":"Finish all my tasks before the due date.",
+        "october":"Enjoy the time with my family every day.",
+        "november":"Start a personal project.",
+        "december":"Celebrate Christmas and New Year."
+    } 
+    
+    
+    
+    
+
+# TAREA
+'''
 def january(request):
     return HttpResponse("Walk for at least 30 min every day.")
 
@@ -40,3 +91,4 @@ def november(request):
 
 def december(request):
     return HttpResponse("Celebrate Christmas and New Year.")
+'''
